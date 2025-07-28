48798352fbac809a97f091691c4e5f2710335eea
irish1814 <ihouri@g.jct.ac.il>
2025-07-28 20:05:07 +0300

    Added basic routes and controllers

---
+++ routes/auth.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const authRoutes = Router();
+
+authRoutes.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(401).send({ error: 'Email and Password are required' });
+    }
+    if(email === 'admin@gmail.com' && password === 'password1234') {
+        res.status(200).send({ message: 'login successfully' });
+    } else {
+        res.status(403).send({message: 'Invalid email or password'});
+    }
+});
+
+export default authRoutes;
